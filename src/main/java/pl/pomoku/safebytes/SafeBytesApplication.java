0a74b57cbdba1f9bed2479167c680901f7266726
hubirto <hubert.rybicki.hr@gmail.com>
2024-09-25 18:44:33 +0200

    Initial commit

---
+++ src/main/java/pl/pomoku/safebytes/SafeBytesApplication.java
@@ -0,0 +1,13 @@
+package pl.pomoku.safebytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeBytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeBytesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pomoku/safebytes/SafeBytesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pomoku.safebytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeBytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
