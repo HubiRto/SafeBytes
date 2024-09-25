dd2a8e97228ef21646576397c7eb4ee9fa27d013
hubirto <hubert.rybicki.hr@gmail.com>
2024-09-25 18:56:38 +0200

    add config

--- src/main/java/pl/pomoku/safebytes/SafeBytesApplication.java
+++ src/main/java/pl/pomoku/safebytes/SafeBytesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SafeBytesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SafeBytesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SafeBytesApplication.class, args);
+    }
 
 }
--- src/test/java/pl/pomoku/safebytes/SafeBytesApplicationTests.java
+++ src/test/java/pl/pomoku/safebytes/SafeBytesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SafeBytesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
