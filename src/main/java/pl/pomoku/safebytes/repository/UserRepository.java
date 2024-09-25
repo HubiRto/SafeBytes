824753a4755f163bb274e06dae7461893e117789
hubirto <hubert.rybicki.hr@gmail.com>
2024-09-25 19:33:05 +0200

    add user repo

---
+++ src/main/java/pl/pomoku/safebytes/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package pl.pomoku.safebytes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.pomoku.safebytes.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
